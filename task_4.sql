4e212f1f2cdfa4173a0f1c6d144295855e36f5dc
Developer-Linus <linusmksu30@gmail.com>
2025-01-29 17:55:26 +0300

    completed task 4

--- task_4.sql
+++ task_4.sql
@@ -14,11 +14,11 @@ USE alx_book_store;
 mycursor = mydb.cursor()
 
 -- Write SQL query to fetch table structure
-table_name = "Books"
+TABLE_NAME = "Books"
 sql = f"""
-SELECT COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH, IS_NULLABLE, COLUMN_KEY
+SELECT COLUMN_NAME, COLUMN_TYPE, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH, IS_NULLABLE, COLUMN_KEY 
 FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = '{table_name}'
+WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = '{TABLE_NAME}'
 ORDER BY ORDINAL_POSITION
 """
 -- Execute the SQL QUERY
@@ -26,13 +26,14 @@ mycursor.execute(sql)
 
 -- Print the table structure
 print(f"Structure of table `{table_name}`:")
-print("------------------------------------------------")
-print(f"{'Column Name':<20}{'Data Type':<15}{'Max Length':<15}{'Nullable':<10}{'Key':<10}")
-print("------------------------------------------------")
+print("----------------------------------------------------------------------------")
+print(f"{'Column Name':<20}{'Column Type':<20}{'Data Type':<15}{'Max Length':<15}{'Nullable':<10}{'Key':<10}")
+print("----------------------------------------------------------------------------")
 
 -- Fetch and display the results
-for column_name, data_type, max_length, is_nullable, column_key in mycursor:
-    print(f"{column_name:<20}{data_type:<15}{max_length:<15}{is_nullable:<10}{column_key:<10}")
+for row in mycursor.fetchall():
+    column_name, column_type, data_type, char_max_length, is_nullable, column_key = row
+    print(f"{column_name:<20}{column_type:<20}{data_type:<15}{str(char_max_length):<15}{is_nullable:<10}{column_key:<10}")
 
 -- Close the cursor and database connection
 mycursor.close()
